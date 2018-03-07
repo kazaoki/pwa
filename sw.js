884484a6529bf98f9e8ef51fbf0e30ec3436346f
Tatsuya Karino <karino@kazaoki.jp>
2018-03-07 22:19:26 +0900

    [update] キャッシュロードをシンプルに。

--- sw.js
+++ sw.js
@@ -26,20 +26,7 @@ self.addEventListener('fetch', function(event) {
 		caches
 			.match(event.request)
 			.then(function(response) {
-				if (response) return response;
-				var fetchRequest = event.request.clone();
-				return fetch(fetchRequest).then(
-					function(response) {
-						if(!response || response.status !== 200 || response.type !== 'basic') return response;
-						var responseToCache = response.clone();
-						caches.open(CACHE_NAME)
-							.then(function(cache) {
-								cache.put(event.request, responseToCache);
-							}
-						);
-						return response;
-					}
-				);
+				return response ? response : fetch(event.request);
 			}
 		)
 	);
