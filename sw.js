5487a8e93dce1d913eb9cd130f8a13a3b9927cbe
Tatsuya Karino <karino@kazaoki.jp>
2018-03-07 22:30:18 +0900

    [cont] test

--- sw.js
+++ sw.js
@@ -20,14 +20,13 @@ self.addEventListener('install', function(event) {
 	);
 });
 
-// リソースフェッチ時のキャッシュロード処理
-self.addEventListener('fetch', function(event) {
-	event.respondWith(
-		caches
-			.match(event.request)
-			.then(function(response) {
-				return response ? response : fetch(event.request);
-			}
-		)
-	);
-});
+// // リソースフェッチ時のキャッシュロード処理
+// self.addEventListener('fetch', function(event) {
+// 	event.respondWith(
+// 		caches
+// 			.match(event.request)
+// 			.then(function(response) {
+// 				return response ? response : fetch(event.request);
+// 			})
+// 	);
+// });
