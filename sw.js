7e4fa79e243b2561b873aeed205b3e4a7b356202
Tatsuya Karino <karino@kazaoki.jp>
2018-03-07 22:31:15 +0900

    [cont] test2

--- sw.js
+++ sw.js
@@ -9,24 +9,24 @@ var urlsToCache = [
 	'/pwa/main.js'
 ];
 
-// インストール処理
-self.addEventListener('install', function(event) {
-	event.waitUntil(
-		caches
-			.open(CACHE_NAME)
-			.then(function(cache) {
-				return cache.addAll(urlsToCache);
-			})
-	);
-});
-
-// // リソースフェッチ時のキャッシュロード処理
-// self.addEventListener('fetch', function(event) {
-// 	event.respondWith(
+// // インストール処理
+// self.addEventListener('install', function(event) {
+// 	event.waitUntil(
 // 		caches
-// 			.match(event.request)
-// 			.then(function(response) {
-// 				return response ? response : fetch(event.request);
+// 			.open(CACHE_NAME)
+// 			.then(function(cache) {
+// 				return cache.addAll(urlsToCache);
 // 			})
 // 	);
 // });
+
+// リソースフェッチ時のキャッシュロード処理
+self.addEventListener('fetch', function(event) {
+	event.respondWith(
+		caches
+			.match(event.request)
+			.then(function(response) {
+				return response ? response : fetch(event.request);
+			})
+	);
+});
